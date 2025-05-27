969cec12989298937b42b5b544c59f3beb2fe436
unknown <josedavidfimu@gmail.com>
2025-05-26 19:48:07 -0600

    feat: se agrego el metodo patch a la configuracion de cors

--- configs/server.js
+++ configs/server.js
@@ -22,7 +22,7 @@ const middlewares = (app) => {
     app.use(express.json());
     app.use(cors({
     origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
